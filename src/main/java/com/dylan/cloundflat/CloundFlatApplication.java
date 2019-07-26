a0c9f8a54c973b9119b14870753b57aa62ef48bf
yangqc <374866976@qq.com>
2019-07-26 17:50:57 +0800

    第一个版本,第一次提交

---
+++ src/main/java/com/dylan/cloundflat/CloundFlatApplication.java
@@ -0,0 +1,13 @@
+package com.dylan.cloundflat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloundFlatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloundFlatApplication.class, args);
+    }
+
+}
