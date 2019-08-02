6b32ff8001a2155978a073174c0dbcb5bfa5db6f
yangqc <374866976@qq.com>
2019-08-02 16:56:49 +0800

    robbin增加 断路器（Hystrix）当service-demo访问失败时,则返回错误信息。而不是一直无响应

--- service-robbin/src/main/java/com/dylan/servicerobbin/controler/HelloControler.java
+++ service-robbin/src/main/java/com/dylan/servicerobbin/controler/HelloControler.java
@@ -17,5 +17,4 @@ public class HelloControler {
         return helloService.hiService(name);
     }
 
-
 }
--- service-robbin/src/main/java/com/dylan/servicerobbin/service/HelloService.java
+++ service-robbin/src/main/java/com/dylan/servicerobbin/service/HelloService.java
@@ -1,6 +1,7 @@
 package com.dylan.servicerobbin.service;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +12,13 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-DEMO/hi?name="+name,String.class);
     }
 
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
 }
 
